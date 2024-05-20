b87e3004a95312dd98388263e524c313f91de874
Vladimir <166504502+yarancelo@users.noreply.github.com>
2024-05-20 22:31:12 +0300

    Add files via upload

---
+++ configs/dhcpd/users.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+hostname=$(cat /etc/hostname)
+pass="P@ssw0rd"
+
+if [[ $hostname == "CLI" || $hostname == "HQ-SRV" || $hostname == "HQ-R" ]]; then
+	echo "create admin";
+	useradd admin -m -c "Admin" -U;
+	printf "$pass\n$pass\n" | passwd admin
+fi
+
+if [[ $hostname == "BR-SRV" || $hostname == "BR-R" ]]; then
+	echo "create br-admin";
+	useradd br-admin -m -c "Branch admin" -U;
+	printf "$pass\n$pass\n" | passwd br-admin
+fi
+
+if [[ $hostname == "HQ-R" || $hostname == "BR-R" || $hostname == "BR-SRV" ]]; then
+	echo "create net-admin";
+	useradd net-admin -m -c "Network admin" -U;
+	printf "$pass\n$pass\n" | passwd net-admin
+fi
